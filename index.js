8de3e93971ca098ce1ee4503939f616d462dcd6e
Dandelionsvv <131456358+Dandelionsvv@users.noreply.github.com>
2023-09-18 18:27:15 +0400

    davaleba1

---
+++ index.js
@@ -0,0 +1,67 @@
+//ცვლადის მნიშვნელობაა 100
+
+// let a = 100
+
+
+// if (a < 50){
+//     console.log('a is not less than 50')
+// }
+// else if (a > 20){
+//     console.log('a is greater than 20')
+// }
+// else{
+//     console.log('null')
+// }
+
+// ternary operator version
+// a < 50? console.log ('a is greater than 50'): console.log('a is not less than 50')
+// a > 20? console.log ('a is greater than 20'): console.log('null')
+
+
+//--------------------------------------------------------
+
+// ცვლადის მნიშვნელობაა მარიამ
+
+// let saxeli = "mariam"
+
+// if (saxeli === "mariam"){
+//     console.log ('true')
+// }
+// else (
+//     console.log ('false')
+// )
+
+// ternary operator version
+// saxeli === "mariam"? console.log('true'): console.log('false')
+
+
+//--------------------------------------------------------
+
+//ცვლადის მნიშვნელობა მარიამ - switch case
+
+// let saxeli = "mariam"
+
+// switch(saxeli){
+//     case "mariam":
+//         console.log('true')
+//         break;
+//     case "nini":
+//         console.log('false')
+//         break;
+// }
+
+
+//--------------------------------------------------------
+
+//ცვლადის ტიპი
+
+// let mshia = 'gnocchi'
+// console.log(typeof mshia)
+
+
+//--------------------------------------------------------
+
+//გადაყვანა სტრინგში
+
+// let number = 10
+// console.log(number.toString())
